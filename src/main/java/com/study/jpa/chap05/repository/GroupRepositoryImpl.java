2451415f5b63d0ecfa187c12097e57ab691e7212
soongu <naong526@naver.com>
2025-02-12 12:50:18 +0900

    # chap05-13. 커스텀 레포지토리로 jdbc template 연동

--- src/main/java/com/study/jpa/chap05/repository/GroupRepositoryImpl.java
+++ src/main/java/com/study/jpa/chap05/repository/GroupRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.study.jpa.chap05.repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.study.jpa.chap05.dto.GroupAverageResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,17 +16,40 @@ public class GroupRepositoryImpl implements GroupRepositoryCustom {
     // querydsl을 사용하게 해주는 의존객체
     private final JPAQueryFactory factory;
 
-    // querydsl로 구현
+    // jdbc를 위한 의존객체
+    private final JdbcTemplate template;
+
+    // JDBC로 구현
     @Override
     public List<GroupAverageResponse> groupAverage() {
-        return factory
-                .select(idol.group.groupName, idol.age.avg())
-                .from(idol)
-                .groupBy(idol.group)
-                .fetch()
-                .stream()
-                .map(GroupAverageResponse::from)
-                .collect(Collectors.toList())
-                ;
+        String sql = """
+                SELECT 
+                    G.group_name AS groupName
+                    , AVG(I.age) AS averageAge
+                FROM tbl_idol I
+                JOIN tbl_group G
+                ON I.group_id = G.group_id
+                GROUP BY G.group_id
+                """;
+        return template.query(sql,
+                (rs, n)
+                        -> GroupAverageResponse.builder()
+                        .groupName(rs.getString("groupName"))
+                        .averageAge(rs.getDouble("averageAge"))
+                        .build());
     }
+
+    // querydsl로 구현
+//    @Override
+//    public List<GroupAverageResponse> groupAverage() {
+//        return factory
+//                .select(idol.group.groupName, idol.age.avg())
+//                .from(idol)
+//                .groupBy(idol.group)
+//                .fetch()
+//                .stream()
+//                .map(GroupAverageResponse::from)
+//                .collect(Collectors.toList())
+//                ;
+//    }
 }
